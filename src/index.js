a7c7a3362cc9a5f10e995501fa3ae752415261d5
Kudesniza <kudesniza@yandex.ru>
2020-10-14 20:41:57 +0300

    init: morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,9 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+  let start = 0;
+  let arr = [];
+  let localLetter = '';
+  for (let i=10; i<=expr.length; i+=10) {
+    let x = expr.slice(start, i);
+    if (x.includes("*")) {
+      arr.push(" ");
+    } else {
+      for (let j=0; j<10; j+=2 ) {
+        if (x.slice(j, j + 2) === '10') {
+          localLetter += '.';
+        }
+        if (x.slice(j, j + 2) === '11') {
+          localLetter += '-'
+       }
+     }
+    }
+    arr.push(MORSE_TABLE[localLetter])
+    localLetter = ''
+    start += 10;
+  }
+  return arr.join('');
+} 
 
 module.exports = {
-    decode
+  decode
 }
